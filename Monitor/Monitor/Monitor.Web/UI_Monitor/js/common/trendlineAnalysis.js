2cc8fcaf917ca65e94bc0d2dc6c1551079803774
decrula <decrula@gmail.com>
2015-01-19 11:31:04 +0800

    修改过程选择器的PAGEID

--- Monitor/Monitor/Monitor.Web/UI_Monitor/js/common/trendlineAnalysis.js
+++ Monitor/Monitor/Monitor.Web/UI_Monitor/js/common/trendlineAnalysis.js
@@ -234,7 +234,7 @@ function addCurrentAchorToTagList() {
 
 function popupAddTagItemWindow() {
     $('#tagSelectorWindow').dialog('open');
-    $('#tagSelector').attr('src', '/UI_Monitor/ProcessEnergyMonitor/TrendLineAnalysis/ProcessSelector.aspx');
+    $('#tagSelector').attr('src', '/UI_Monitor/ProcessEnergyMonitor/TrendLineAnalysis/ProcessSelector.aspx?PageId=5CE25714-15AE-490B-947E-13C28BA20316');
 }
 
 // 弹出简单趋势
